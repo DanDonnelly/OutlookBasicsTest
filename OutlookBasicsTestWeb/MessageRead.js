caf8b4211e1ff97580744b611981ce2834f93196
Dan Donnelly <d.donnelly.bliss@gmail.com>
2020-05-02 17:51:50 +0100

    Add project files.

---
+++ OutlookBasicsTestWeb/MessageRead.js
@@ -0,0 +1,33 @@
+﻿(function () {
+  "use strict";
+
+    var divBotOpen = true;
+
+  // The Office initialize function must be run each time a new page is loaded.
+  Office.initialize = function (reason) {
+    $(document).ready(function () {
+
+        //divBot = document.querySelector('#divBottom');
+
+        var btn1 = document.querySelector('#btn1');
+        btn1.addEventListener("click", btn1Click);
+        btn1.textContent = "Ready to ";
+
+        
+  
+    });
+  };
+
+
+    function btn1Click() {
+
+        if (divBotOpen)
+            $('#divBottom').hide("slow");
+        else
+            $('#divBottom').show("slow");
+
+        divBotOpen = !divBotOpen;
+    }
+
+ 
+})();
